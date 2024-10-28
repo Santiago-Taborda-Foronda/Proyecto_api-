25af0da1198344a85e4017b882c2a0b06548e9e9
Sena <SENA@ARMSCCCCTGOD155.FormacionSena.RED>
2024-10-28 15:12:24 -0500

    Faltan dos puntos de la tarea

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
-const environments = [
-    {
-        name: "Rick Sanchez",
-        status: "Alive",
-        species: "Human",
-        image: "assets/images/hamster.png"
-    },
-    {
-        name: "Morty Smith",
-        status: "Alive",
-        species: "Human",
-        image: "assets/images/hamster.png"
-    }
-    // Puedes agregar más personajes aquí
-];
+const url='https://rickandmortyapi.com/api/character'
 
-window.addEventListener('DOMContentLoaded', makecards)
+const getCharacter = async (URL) =>{
+    const response=await fetch(URL)
+    const data= await response.json()
+    console.log(data.results.length)
+
+    runs(data)
+  
+}
+
+getCharacter(url)
+
+
+window.addEventListener('DOMContentLoaded', getCharacter)
 
 function makecard(card) {
     const container = document.createElement('div'); 
-    container.id = 'container-title'
-    container.classList.add('container-title');
-
+    container.id = 'container'
+    container.classList.add('card');
 
     const imgCard = document.createElement('img')
-    imgCard.src = card.img
-    imgCard.alt= card.Description
+    imgCard.src = card.image
+    imgCard.alt= card.name
+
+    const containerDescription = document.createElement('div')
+    containerDescription.id = 'container-title'
+
 
     const firstTitleCard  = document.createElement('h2');
-    firstTitleCard.textContent= card.title
-    
-    const SecondTitleCard  = document.createElement('h3');
-    SecondTitleCard.textContent= card.title
+    firstTitleCard.textContent= card.name
 
-    const ThirdTitleCard  = document.createElement('h3');
-    ThirdTitleCard.textContent= card.title
+    const SecondTitleCard  = document.createElement('h3');
+    SecondTitleCard.textContent= card.species
+
+    const ThirdTitleCard  = document.createElement('h4');
+    ThirdTitleCard.textContent= card.status
 
     container.appendChild(imgCard)
-    container.appendChild(firstTitleCard)
-    container.appendChild(SecondTitleCard)
-    container.appendChild(ThirdTitleCard)
-    
+    container.appendChild(containerDescription)
+
+    containerDescription.appendChild(firstTitleCard)
+    containerDescription.appendChild(SecondTitleCard)
+    containerDescription.appendChild(ThirdTitleCard)
+
 
     document.querySelector('main').appendChild(container)
 
-    
+
 }
 
-
-function makecards() {
-   for (let index = 0; index < 12; index++) {
-        makecard(environments);
-        console.log(environments)
-        // environments.forEach(card=>makecard(card))
-    }
+function runs (data){
+    data.results.forEach(character => {
+        makecard(character)
+    });
 }
